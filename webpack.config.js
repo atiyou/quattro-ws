7149f5f660fa4250513e89e6fe5e3f7e81903339
atiyou <youssef.atik@gmail.com>
2017-10-18 01:25:31 +0200

    using webpack & some loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
@@ -11,15 +12,21 @@ var config = {
     filename: 'bundle.js'
   },
   module : {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        include: APP_DIR,
-        exclude : /node_modules/,
-        loader: 'babel'
-      }
-    ]
-  }
+  	rules: [
+  	  { test: /\.txt$/, use: 'raw-loader'},
+  	  {
+  	  	test: /\.(js|jsx)$/, 
+  	  	include: APP_DIR,
+  	  	exclude: /node_modules/,
+  	  	use: 'babel-loader'
+  	  },
+  	  { test: /\.css$/, use: 'css-loader'}
+  	]
+  },
+  plugins: [
+  	  new webpack.optimize.UglifyJsPlugin(),
+  	  new HtmlWebpackPlugin({template: './src/client/index.html'})
+  ]
 
 };
 
